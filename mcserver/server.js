2410fefd8d37732e8e5f24439c2f2cf1ed54387a
Muaku <muaku12@gmail.com>
2018-01-16 14:46:28 +0900

    first commit

---
+++ mcserver/server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const mqtt = require('mqtt')
+const _ = require("lodash")
+const Broker = require("./config").Broker
+const MongoDB = require("./config").MongoDB
+const mongodbURL = `mongodb://localhost/${MongoDB.dbName}`
+mongoose.connect(mongodbURL, {useMongoClient: true})    /* Connect to mongodb */
+const micro = require("./model").micro
+/* Connect to mqtt broker */
+const client  = mqtt.connect(`mqtts://${Broker.Server}:${Broker.SSL_Port}`,{username: Broker.User, password: Broker.Password})
+
+client.on('connect', function(ck) {
+  console.log('Connected to Broker')
+});
+client.on('error', function(err) {
+  console.log('Connecte error: ', err)
+});
+
+client.subscribe('#');
+
+client.on('message', function(topic, message) {
+  console.log(topic, JSON.parse(message))
+    /* TODO save message data to DB */
+    if(!_.isEmpty(JSON.parse(message))) {
+        var microData = new micro(JSON.parse(message))
+        microData.save()
+    }
+})
+
+const PORT = 9000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
